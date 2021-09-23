d0998395355b0ff0192132aa0fba1865063ecbb5
CamiloSinningUN <61607058+CamiloSinningUN@users.noreply.github.com>
2021-09-23 00:13:33 -0500

    Crud_beta

--- src/index.js
+++ src/index.js
@@ -2,15 +2,12 @@ const express = require('express');
 const app = express();
 require('path');
 
-//Settings 
 app.set('port',3000);
 
-//routes
 app.set('/',(req,res)=>{
     res.sendFile(path.join(__dirname,'views/index.html'))
 })
 
-//listening server
 app.listen(app.get('port'),()=>{
     console.log('Sevidor inciado')
 })
