7a185dadba31ea3904836574e8db41c73d6655a6
CamiloSinningUN <61607058+CamiloSinningUN@users.noreply.github.com>
2021-09-25 16:31:07 -0500

    Revert "error"
    This reverts commit 8130fac73efa5ffef271bac77412d8336e263724.

--- src/routes/hijo.js
+++ src/routes/hijo.js
@@ -2,9 +2,8 @@ const express = require('express');
 const router = express.Router();
 const hijoController = require('../controllers/hijoController');
 
+router.get('/',hijoController.list);
 router.post('/add_hijo', hijoController.save);
 router.get('/delete_hijo/:id',hijoController.delete);
-router.get('/update_hijo/:id',hijoController.edit);
-router.post('/update_hijo/:id',hijoController.update);
 
 module.exports = router;
--- src/routes/indice.js
+++ src/routes/indice.js
@@ -5,8 +5,5 @@ const indiceController = require('../controllers/indiceController');
 router.get('/',indiceController.list);
 router.get('/hijos',indiceController.crud_hijos);
 router.get('/padres',indiceController.crud_padres);
-router.get('/hijos_edit',indiceController.hijos_edit);
-router.get('/padres_edit',indiceController.padres_edit);
-
 
 module.exports = router;
