fd705e7bbb8ccf76dfd295221715d418ec0a44e5
CamiloSinningUN <61607058+CamiloSinningUN@users.noreply.github.com>
2021-09-25 19:26:44 -0500

    edit and validations and front end changes

--- src/routes/padre.js
+++ src/routes/padre.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 const padreController = require('../controllers/padreController');
 
-router.get('/',padreController.list);
 router.post('/add_padre', padreController.save);
 router.get('/delete_padre/:id',padreController.delete);
+router.post('/update_padre/:id',padreController.update);
 
 module.exports = router;
