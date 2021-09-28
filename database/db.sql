dd4e5d14a9a127f00f1999eea5affd3a415ea3a4
CamiloSinningUN <61607058+CamiloSinningUN@users.noreply.github.com>
2021-09-27 23:22:09 -0500

    Revert "para revert"
    This reverts commit 8bcf472107f0ba14261c54b256f7051ca1bb694e.

--- database/db.sql
+++ database/db.sql
@@ -15,5 +15,14 @@ CREATE TABLE hijo (
     FOREIGN KEY(hijode) REFERENCES padre(id) ON DELETE CASCADE
 );
 
+ /* Padre */
+INSERT INTO padre VALUES (12345, 'Andres');
+INSERT INTO padre VALUES (23456, 'Carlos');
+INSERT INTO padre VALUES (34567, 'Esteban');
 INSERT INTO padre VALUES (0, 'No tiene padres');
-
+ 
+ /* Hijo */
+INSERT INTO hijo VALUES (1, 'Ana', 12345);
+INSERT INTO hijo VALUES (2, 'Camilo', 34567);
+INSERT INTO hijo VALUES (3, 'Eduardo', 23456);
+INSERT INTO hijo VALUES (4, 'Isabella',0);
